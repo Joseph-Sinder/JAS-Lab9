069ec1e849c10b59054d3cb14f4b2c060b59ddd8
DuaneHuynh <dhuynh1@ufl.edu>
2024-04-11 13:08:56 -0400

    Updated

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ def encode(password):
         else:
             encoded_password += str((int(password[i]) - 7))
     return encoded_password
+
+def decode(password):
+    decoded_password = ''
+    for char in password:
+        decoded_password += str((int(char) + 7) % 10)
+    return decoded_password
+
