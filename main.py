b69ead19bf0006c9f1b970c1e6217ada7c5a1e5e
Joseph-Sinder <jsinder@ufl.edu>
2024-04-11 12:43:18 -0400

    Encode
    Encoded Code

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is Joseph Sinder's main.py file.
+
+def encode(password):
+    encoded_password = ''
+    for i in range(0, len(password)):
+        if int(password[i]) < 7:
+            encoded_password += str((int(password[i]) + 3))
+        else:
+            encoded_password += str((int(password[i]) - 7))
+    print("Your password has been encoded and stored!")
